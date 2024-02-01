0a2ead74791a0a9e564f35dca8fb8d8c60218835
Elisa Yan <elisa.yan@studio.unibo.it>
2024-02-01 21:21:28 +0100

    improved a02c e2

--- src/a02c/e2/LogicsImpl.java
+++ src/a02c/e2/LogicsImpl.java
@@ -4,80 +4,70 @@ import java.util.*;
 
 public class LogicsImpl implements Logics {
 
-    private final int size;
     private List<Pair<Integer, Integer>> contour = new LinkedList<>();
     private List<Pair<Integer, Integer>> angle = new LinkedList<>();
+    private int size;
 
     public LogicsImpl(int size) {
         this.size = size;
     }
 
     @Override
-    public void hit(int x, int y) {
-        Pair<Integer, Integer> pair = new Pair<Integer, Integer>(x, y);
+    public boolean hit(int x, int y) {
+        Pair<Integer, Integer> actual = new Pair<>(x, y);
 
         if (contour.isEmpty()) {
-            expandContour(x-1, y-1, x+1, y+1);
+            contour = setContour(x - 1, y - 1, x + 1, y + 1);
+            calculateAngle();
+            return true;
         }
 
-        if (!contour.isEmpty() && angle.contains(pair)) {
-            if (pair.equals(angle.get(0))) {
-                expandContour(x-1, y-1, angle.get(3).getX(), angle.get(3).getY());
-            }
-
-            if (pair.equals(angle.get(1))) {
-                expandContour(x-1, y+1, angle.get(2).getX(), angle.get(2).getY());
-            }
-
-            if (pair.equals(angle.get(2))) {
-                expandContour(x+1, y-1, angle.get(1).getX(), angle.get(1).getY());
-            }
-
-            if (pair.equals(angle.get(3))) {
-                expandContour(x+1, y+1, angle.get(0).getX(), angle.get(0).getY());
-            }
+        if (angle.contains(actual)) {
+            contour = extendContour(x, y);
+            calculateAngle();
+            return true;
         }
+        return false;
     }
 
-    private void setAngle() {
-        List<Integer> listX = new LinkedList<>();
-        List<Integer> listY = new LinkedList<>();
-        
+    private void calculateAngle() {
+        List<Integer> xList = new ArrayList<>();
+        List<Integer> yList = new ArrayList<>();
 
         for (Pair<Integer, Integer> pair : contour) {
-            listX.add(pair.getX());
-            listY.add(pair.getY());
+            xList.add(pair.getX());
+            yList.add(pair.getY());
         }
 
-        int minX = listX.stream().min(Integer::compareTo).orElseThrow();
-        int minY = listX.stream().min(Integer::compareTo).orElseThrow();
-        int maxX = listX.stream().max(Integer::compareTo).orElseThrow();
-        int maxY = listX.stream().max(Integer::compareTo).orElseThrow();
-        
+        int minX = Collections.min(xList);
+        int maxX = Collections.max(xList);
+        int minY = Collections.min(yList);
+        int maxY = Collections.max(yList);
+
         angle.clear();
-        angle.add(new Pair<Integer, Integer>(minX, minY));
-        angle.add(new Pair<Integer, Integer>(minX, maxY));
-        angle.add(new Pair<Integer, Integer>(maxX, minY));
-        angle.add(new Pair<Integer, Integer>(maxX, maxY));
+        angle.add(new Pair<>(minX, minY));
+        angle.add(new Pair<>(minX, maxY));
+        angle.add(new Pair<>(maxX, minY));
+        angle.add(new Pair<>(maxX, maxY));
     }
 
-    private void expandContour(int x, int y, int x2, int y2) {
-        contour.clear();
-        for (int i = Math.min(x2, x); i <= Math.max(x2, x); i++) {
+    private List<Pair<Integer, Integer>> extendContour(int x, int y) {
+        Pair<Integer, Integer> topLeft = new Pair<>(angle.get(0).getX() - 1, angle.get(0).getY() - 1);
+        Pair<Integer, Integer> bottomRight = new Pair<>(angle.get(3).getX() + 1, angle.get(3).getY() + 1);
+        return setContour(topLeft.getX(), topLeft.getY(), bottomRight.getX(), bottomRight.getY());
+    }
+
+    private List<Pair<Integer, Integer>> setContour(int x, int y, int x2, int y2) {
+        List<Pair<Integer, Integer>> myContour = new LinkedList<>();
+
+        for (int i = Math.min(x, x2); i <= Math.max(x, x2); i++) {
             for (int j = Math.min(y, y2); j <= Math.max(y, y2); j++) {
                 if (i == x || i == x2 || j == y || j == y2) {
-                    contour.add(new Pair<Integer, Integer>(i, j));
+                    myContour.add(new Pair<>(i, j));
                 }
-
             }
         }
-        setAngle();
-    }
-
-    @Override
-    public boolean isOver() {
-        return contour.stream()
-                .anyMatch(e -> e.getX() == 0 || e.getY() == 0 || e.getX() == size - 1 || e.getY() == size - 1);
+        return myContour;
     }
 
     @Override
@@ -85,4 +75,9 @@ public class LogicsImpl implements Logics {
         return contour.contains(new Pair<>(x, y));
     }
 
+    @Override
+    public boolean isOver() {
+        return contour.stream()
+                .anyMatch(e -> e.getX() == -1 || e.getY() == -1 || e.getX() == size || e.getY() == size);
+    }
 }
