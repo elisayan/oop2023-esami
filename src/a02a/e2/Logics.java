b7e75ae3e5f876db5ed63f7e94d72d57f6891fc6
Elisa Yan <elisa.yan@studio.unibo.it>
2024-02-01 22:54:48 +0100

    implemented a02a e2

---
+++ src/a02a/e2/Logics.java
@@ -0,0 +1,7 @@
+package a02a.e2;
+
+public interface Logics {
+    boolean isAsterisch(int x, int y);
+    boolean hit(int x, int y);
+    boolean isOver();
+}
---
+++ src/a02a/e2/LogicsImpl.java
@@ -0,0 +1,65 @@
+package a02a.e2;
+
+import java.util.*;
+
+public class LogicsImpl implements Logics{
+
+    private final int size;
+    private List<Pair<Integer, Integer>> asterisch = new LinkedList<>();
+    private List<Pair<Integer, Integer>> selected = new LinkedList<>();
+
+    public LogicsImpl(int size) {
+        this.size = size;
+    }
+
+    private void setAsterisch(){
+        for (int i = 1; i < size; i+=2) {
+            for (int j = 0; j < size; j++) {
+                asterisch.add(new Pair<Integer,Integer>(i, j));
+            }
+        }
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 1; j < size; j+=2) {
+                asterisch.add(new Pair<Integer,Integer>(i, j));
+            }
+        }
+    }
+
+    @Override
+    public boolean isAsterisch(int x, int y) {
+        setAsterisch();
+        return asterisch.contains(new Pair<>(x,y));
+    }
+
+    @Override
+    public boolean hit(int x, int y) {
+        if (!asterisch.contains(new Pair<>(x,y))) {
+            selected.add(new Pair<Integer,Integer>(x, y));
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isOver() {
+        if (selected.size()>=4) {
+            List<Pair<Integer, Integer>> lastFour = selected.subList(selected.size()-4, selected.size());
+            List<Integer> xList = new LinkedList<>();
+            List<Integer> yList = new LinkedList<>();
+            for (Pair<Integer,Integer> pair : lastFour) {
+                xList.add(pair.getX());
+                yList.add(pair.getY());
+            }
+
+            xList= xList.stream().distinct().sorted().toList();
+            yList = yList.stream().distinct().sorted().toList();
+
+            return (xList.size() == 2 && yList.size() == 2) && (xList.get(0).equals(xList.get(xList.size() - 1) - 2))
+                    && (yList.get(0).equals(yList.get(yList.size() - 1) - 2));
+        }
+        return false;
+    }
+    
+}
